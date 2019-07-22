9705076865c303cbd86f901ca4e34f3a43c2c1d7
vinit134 <vinit134@users.noreply.github.com>
2019-07-22 13:47:19 +0200

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 NAME="chatbot_ner"                                              # Name of the application
-DJANGODIR=chatbot_ner                                         # Django project directory
+DJANGODIR=./chatbot_ner                                         # Django project directory
 SOCKFILE=run/gunicorn.sock                                    # we will communicate using this unix socket
 USER=`whoami`                                                   # the user to run as
 GROUP=`id -gn`                                                  # the group to run as
 NUM_WORKERS=4                                                   # how many worker processes should Gunicorn spawn
 DJANGO_SETTINGS_MODULE=chatbot_ner.settings                     # which settings file should Django use
 DJANGO_WSGI_MODULE=chatbot_ner.wsgi                             # WSGI module name
-PORT=8081
+PORT=8888
 TIMEOUT=600
 
 echo `pwd`
@@ -30,23 +30,23 @@ export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 RUNDIR=$(dirname $SOCKFILE)
 test -d $RUNDIR || mkdir -p $RUNDIR
 
-pip install -r runtime.txt
+#pip install -r requirements.txt
 
 echo "copying config & run import data"
-cd chatbot_ner/
+cd ./chatbot_ner/
 cp config.example config
 
 cd ..
-python initial_setup.py
+./initial_setup.py
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
 exec gunicorn ${DJANGO_WSGI_MODULE}:application \
-  -b 0.0.0.0:$PORT \
+  -b localhost:$PORT \
   --name $NAME \
   --workers $NUM_WORKERS \
   --user=$USER --group=$GROUP \
   --log-level=debug \
   --bind=unix:$SOCKFILE \
   --timeout $TIMEOUT \
-  --backlog=2048
+  --backlog=2048 &
 #  --threads=2
